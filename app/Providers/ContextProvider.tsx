077f287b0037fb5084ad5962b3e5431aca73cd69
Alucardya <vladkhalilov@yahoo.com>
2024-06-17 23:01:42 +0300

    it is working yet with out formatate normally

--- app/Providers/ContextProvider.tsx
+++ app/Providers/ContextProvider.tsx
@@ -1,29 +1,35 @@
 "use client";
-import React from 'react'
-import { GlobalProvider } from '../context/globalProvider';
-import toast, { Toaster } from 'react-hot-toast';
+import React from "react";
+import { GlobalProvider } from "../context/globalProvider";
+import { Toaster } from "react-hot-toast";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
+  const [isReady, setIsReady] = React.useState(false);
 
-    const [isReady, setIsReady] = React.useState(false);
-
-    React.useEffect(() => {
+  React.useEffect(() => {
     setTimeout(() => {
-        setIsReady(true);
-    }, 200);
-}, []);
+      setIsReady(true);
+    }, 250);
+  }, []);
 
-if (!isReady) {
-    return null;
-}
-  return <GlobalProvider>
-    <Toaster />
-
-    {children}</GlobalProvider>;
+  if (!isReady) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <span className="loader"></span>
+      </div>
+    );
   }
+
+  return (
+    <GlobalProvider>
+      <Toaster />
+      {children}
+    </GlobalProvider>
+  );
+}
 
 export default ContextProvider;
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,11 @@
 // File path: middleware.ts
 
-<<<<<<< Updated upstream
-import { clerkMiddleware } from '@clerk/nextjs/server'; // Ensure this is the correct import
-=======
 import { authMiddleware } from "@clerk/nextjs/server";
->>>>>>> Stashed changes
 
 console.log("Initializing Clerk Middleware");
 
 export default authMiddleware({});
 
-<<<<<<< Updated upstream
-export default clerkMiddleware({
-  publicRoutes: ["/"],
-});
-
-=======
->>>>>>> Stashed changes
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
