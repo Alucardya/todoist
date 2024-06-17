28f4ad2c862f580c577f9c0a5230b948c4ff6db8
Alucardya <vladkhalilov@yahoo.com>
2024-06-17 17:20:30 +0300

    new mf create task

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -1,8 +1,11 @@
 "use client";
-import axios from 'axios';
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import styled from 'styled-components';
+import { useGlobalState } from "@/app/context/globalProvider";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import styled from "styled-components";
+import Button from "../Button/button"; 
+import { add, plus } from "@/app/utils/Icons";
 function CreateContent() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -10,7 +13,9 @@ function CreateContent() {
   const [completed, setCompleted] = useState(false);
   const [important, setImportant] = useState(false);
 
-  const handleChange = (name) => (e) => {
+  const { theme, allTasks, closeModal } = useGlobalState();
+
+  const handleChange = (name: string) => (e: any) => {
     switch (name) {
       case "title":
         setTitle(e.target.value);
@@ -30,9 +35,9 @@ function CreateContent() {
       default:
         break;
     }
-  }
+  };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     const task = {
@@ -45,10 +50,16 @@ function CreateContent() {
 
     try {
       const res = await axios.post("/api/tasks", task);
+
       if (res.data.error) {
         toast.error(res.data.error);
       }
-      toast.success("Task created successfully.");
+
+      if (!res.data.error) {
+        toast.success("Task created successfully.");
+        allTasks();
+        closeModal();
+      }
     } catch (error) {
       toast.error("Something went wrong.");
       console.log(error);
@@ -56,7 +67,7 @@ function CreateContent() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <CreateContentStyled onSubmit={handleSubmit} theme={theme}>
       <h1>Create a Task</h1>
       <div className="input-control">
         <label htmlFor="title">Title</label>
@@ -66,7 +77,7 @@ function CreateContent() {
           value={title}
           name="title"
           onChange={handleChange("title")}
-          placeholder="e.g., Watch a video from Fireship."
+          placeholder="e.g, Watch a video from Fireship."
         />
       </div>
       <div className="input-control">
@@ -77,7 +88,7 @@ function CreateContent() {
           name="description"
           id="description"
           rows={4}
-          placeholder="e.g., Watch a video from Fireship."
+          placeholder="e.g, Watch a video about Next.js Auth"
         ></textarea>
       </div>
       <div className="input-control">
@@ -85,38 +96,125 @@ function CreateContent() {
         <input
           value={date}
           onChange={handleChange("date")}
+          type="date"
           name="date"
           id="date"
-          type="date"
         />
       </div>
-      <div className="input-control">
+      <div className="input-control toggler">
         <label htmlFor="completed">Toggle Completed</label>
         <input
-          checked={completed}
+          value={completed.toString()}
           onChange={handleChange("completed")}
           type="checkbox"
           name="completed"
           id="completed"
         />
       </div>
-      <div className="input-control">
+      <div className="input-control toggler">
         <label htmlFor="important">Toggle Important</label>
         <input
-          checked={important}
+          value={important.toString()}
           onChange={handleChange("important")}
           type="checkbox"
           name="important"
           id="important"
         />
       </div>
-      <div className="submit-btn">
-        <button type="submit">
-          <span>Submit</span>
-        </button>
+
+      <div className="submit-btn flex justify-end">
+        <Button
+          type="submit"
+          name="Create Task"
+          icon={add}
+          padding={"0.8rem 2rem"}
+          borderRad={"0.8rem"}
+          fw={"500"}
+          fs={"1.2rem"}
+          background={"rgb(0, 163, 255)"}
+        />
       </div>
-    </form>
+    </CreateContentStyled>
   );
 }
+
+const CreateContentStyled = styled.form`
+  > h1 {
+    font-size: clamp(1.2rem, 5vw, 1.6rem);
+    font-weight: 600;
+  }
+
+  color: ${(props) => props.theme.colorGrey1};
+
+  .input-control {
+    position: relative;
+    margin: 1.6rem 0;
+    font-weight: 500;
+
+    @media screen and (max-width: 450px) {
+      margin: 1rem 0;
+    }
+
+    label {
+      margin-bottom: 0.5rem;
+      display: inline-block;
+      font-size: clamp(0.9rem, 5vw, 1.2rem);
+
+      span {
+        color: ${(props) => props.theme.colorGrey3};
+      }
+    }
+
+    input,
+    textarea {
+      width: 100%;
+      padding: 1rem;
+
+      resize: none;
+      background-color: ${(props) => props.theme.colorGreyDark};
+      color: ${(props) => props.theme.colorGrey2};
+      border-radius: 0.5rem;
+    }
+  }
+
+  .submit-btn button {
+    transition: all 0.35s ease-in-out;
+
+    @media screen and (max-width: 500px) {
+      font-size: 0.9rem !important;
+      padding: 0.6rem 1rem !important;
+
+      i {
+        font-size: 1.2rem !important;
+        margin-right: 0.5rem !important;
+      }
+    }
+
+    i {
+      color: ${(props) => props.theme.colorGrey0};
+    }
+
+    &:hover {
+      background: ${(props) => props.theme.colorPrimaryGreen} !important;
+      color: ${(props) => props.theme.colorWhite} !important;
+    }
+  }
+
+  .toggler {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    cursor: pointer;
+
+    label {
+      flex: 1;
+    }
+
+    input {
+      width: initial;
+    }
+  }
+`;
 
 export default CreateContent;
--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,11 @@
-import prisma from "../utils/connect";
+import prisma from "@/app/utils/connect";
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
+
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
@@ -43,76 +44,48 @@ export async function POST(req: Request) {
   }
 }
 
-export async function PUT(req: Request) {
+export async function GET(req: Request) {
   try {
     const { userId } = auth();
+
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
 
-    const { id, title, description, date, completed, important } = await req.json();
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId,
+      },
+    });
 
-    if (!id || !title || !description || !date) {
-      return NextResponse.json({
-        error: "Missing required fields",
-        status: 400,
-      });
-    }
+    return NextResponse.json(tasks);
+  } catch (error) {
+    console.log("ERROR GETTING TASKS: ", error);
+    return NextResponse.json({ error: "Error updating task", status: 500 });
+  }
+}
 
-    if (title.length < 3) {
-      return NextResponse.json({
-        error: "Title must be at least 3 characters long",
-        status: 400,
-      });
+export async function PUT(req: Request) {
+  try {
+    const { userId } = auth();
+    const { isCompleted, id } = await req.json();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
 
     const task = await prisma.task.update({
       where: {
         id,
-        userId,
       },
       data: {
-        title,
-        description,
-        date,
-        isCompleted: completed,
-        isImportant: important,
+        isCompleted,
       },
     });
 
     return NextResponse.json(task);
   } catch (error) {
     console.log("ERROR UPDATING TASK: ", error);
-    return NextResponse.json({ error: "Error updating task", status: 500 });
-  }
-}
-
-export async function DELETE(req: Request) {
-  try {
-    const { userId } = auth();
-    if (!userId) {
-      return NextResponse.json({ error: "Unauthorized", status: 401 });
-    }
-
-    const { id } = await req.json();
-
-    if (!id) {
-      return NextResponse.json({
-        error: "Missing required fields",
-        status: 400,
-      });
-    }
-
-    const task = await prisma.task.delete({
-      where: {
-        id,
-        userId,
-      },
-    });
-
-    return NextResponse.json(task);
-  } catch (error) {
-    console.log("ERROR DELETING TASK: ", error);
     return NextResponse.json({ error: "Error deleting task", status: 500 });
   }
 }
